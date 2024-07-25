c14b703f2bd1b491183089a4c814af6240753d75
Guilherme Linhares <guilherme.linharees@gmail.com>
2024-07-25 19:18:14 -0300

    update version provider kind

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kind = {
       source = "tehcyx/kind"
-      version = "0.0.16"
+      version = "0.5.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
